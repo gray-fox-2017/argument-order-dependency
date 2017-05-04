8f7c5b2c756efbd7fba3f9008873b42d19f416eb
Sidik Hidayatullah <sidik.riders@gmail.com>
2017-05-04 14:17:58 +0700

    release 0 di poles

--- house.js
+++ house.js
@@ -3,19 +3,19 @@
 class House {
 
   constructor(unit) {
-    this.address = unit[address]
-    this.square_feet = unit[square_feet]
-    this.num_bedrooms = unit[num_bedrooms] || 3
-    this.num_baths = unit[num_baths] || 2
-    this.cost = unit[cost] || 320000
-    this.down_payment = unit[down_payment] || 0.20
-    this.sold = unit[sold] || false
-    this.short_sale = unit[short_sale]
-    this.has_tenants = unit[has_tenants] || false
+    this.address = unit.address
+    this.square_feet = unit.square_feet
+    this.num_bedrooms = unit.num_bedrooms || 3
+    this.num_baths = unit.num_baths || 2
+    this.cost = unit.cost || 320000
+    this.down_payment = unit.down_payment || 0.20
+    this.sold = unit.sold || false
+    this.short_sale = unit.short_sale
+    this.has_tenants = unit.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{45}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,16 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let jjib = {}
+  jjib.address = 'Jl. Rempoa Raya No. 5 Bintaro Pesanggrahan Jakarta Selatan 12330';
+  jjib.square_feet= 100,
+  jjib.num_bedrooms= 2,
+  jjib.num_baths= 2,
+  jjib.cost= 'Rp. 13.000.000,- / m2',
+  jjib.down_payment= 12345,
+  jjib.sold= true,
+  jjib.has_tenants= true
+
+const cool = new House(jjib)
 
 console.log(cool.to_s())
