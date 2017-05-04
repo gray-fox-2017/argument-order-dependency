87f8a98a2cc797181164c3bbfe53c0087e6f1192
raynormw <tirtawiryaputra@yahoo.com>
2017-05-04 14:56:58 +0700

    House M.D

--- house.js
+++ house.js
@@ -2,30 +2,48 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(components) {
+    this.address = components["address"];
+    this.square_feet = components["square_feet"];
+    this.num_bedrooms = 3;
+    if (components.hasOwnProperty("num_bedrooms")) {
+      this.num_bedrooms = components["num_bedrooms"];
+    }
+    this.num_baths = 2;
+    if (components.hasOwnProperty("num_baths")) {
+      this.num_baths = components["num_baths"];
+    }
+    this.cost = 320000;
+    if (components.hasOwnProperty("cost")) {
+      this.cost = components["cost"];
+    }
+    this.down_payment = 0.20;
+    if (components.hasOwnProperty("down_payment")) {
+      this.down_payment = components["down_payment"];
+    }
+    this.sold = false;
+    if (components.hasOwnProperty("sold")) {
+      this.sold = components["sold"];
+    }
+    this.short_sale = components["short_sale"];
+    this.has_tenants = false;
+    if (components.hasOwnProperty("has_tenants")) {
+      this.has_tenants = components["has_tenants"];
+    }
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    this.address.replace(/.{10}$/g, '****');
   }
 
   buy(money, good_credit) {
-    if (money >= down_payment && good_credit) {
+    if (money >= this.down_payment && good_credit) {
       this.sold = true
     }
   }
 
   down_payment() {
-    return cost * this.down_payment
+    return this.cost * this.down_payment
   }
 
   to_s() {
