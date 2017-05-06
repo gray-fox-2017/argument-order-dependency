b0e9912cede3a1996fbd17c4372667c6c35917a0
adeanugerah89 <ade_anugerahtb@yahoo.com>
2017-05-06 10:25:43 +0700

    argument-order-dependency done

--- house.js
+++ house.js
@@ -1,38 +1,39 @@
 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+// address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants
+  constructor(part) {
+    this.address = part.address;
+    this.square_feet = part.square_feet;
+    this.num_bedrooms = part.num_bedrooms || 3;
+    this.num_baths = part.num_baths || 2;
+    this.cost = part.cost || 320000;
+    this.down_payment = part.down_payment || 0.20;
+    this.sold = part.sold || false;
+    this.short_sale = part.short_sale;
+    this.has_tenants = part.has_tenants || false;
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+  return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
-    if (money >= down_payment && good_credit) {
+    if (money >= this.down_payment && good_credit) {
       this.sold = true
     }
   }
 
   down_payment() {
-    return cost * this.down_payment
+    return this.cost * this.down_payment
   }
 
   to_s() {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath, ${this.cost}`
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-
+const cool = new House({address: 'Jln.Taman manggis permai no.234', square_feet: 100, num_bedrooms: 2, num_baths: 2, cost: 12345, down_payment: 12345, sold: true, has_tenants: true})
+// console.log(cool);
+// console.log(cool.down_payment());
 console.log(cool.to_s())
